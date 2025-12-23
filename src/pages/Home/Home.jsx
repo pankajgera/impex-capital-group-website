d341d12096c9509f232b2c2af2088cbd34485acf
ashokcrackerdigital <ashok@crackerdigital.com>
2025-12-23 17:55:25 +0530

    added_assets

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div style={{ padding: "40px", textAlign: "center" }}>
+            <h1>Welcome to Impex Capital Group</h1>
+            <p>Home page is working correctly.</p>
+        </div>
+    );
+};
+
+export default Home;
